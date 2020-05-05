72e013ae665a7876642a77920fae89a228c532a8
Aakash Kumar Das <aakashsjr@gmail.com>
2020-05-05 15:24:56 +0530

    updated Wiki filter

--- interests/wikipedia_utils.py
+++ interests/wikipedia_utils.py
@@ -42,23 +42,32 @@ def wikifilter(keyword):
 
 
 def wikicategory(interest):
-    stoplist = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')']
+    symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', ',']
+    stoplist = ['of', 'by', 'lists', 'from', 'articles', 'terms']
     wiki = wikipediaapi.Wikipedia('en')
     categorie = []
+    noise = []
     l = wiki.page(interest)
-    try:
-        a = wiki.categories(l, clshow='!hidden')
-    except:
-        return categorie
+    a = wiki.categories(l, clshow='!hidden')
 
     for k in a.keys():
         cat = k.replace("Category:", "")
         if len(cat.split()) <= 4 and cat != 'Disambiguation pages':
             categorie.append(cat)
 
-    for s in stoplist:
+    for s in symbols:
         for c in categorie:
             if s in c.lower():
-                categorie.remove(c)
+                noise.append(c)
+
+    for c in categorie:
+        for s in stoplist:
+            if s in c.lower().split():
+                noise.append(c)
+
+    noise = list(set(noise))
+
+    for n in noise:
+        categorie.remove(n)
 
     return categorie
