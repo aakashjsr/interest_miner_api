3ac250e0085665603b0ffbeaffa5beccbc2b5ef5
Aakash Kumar Das <aakashsjr@gmail.com>
2020-06-21 20:58:19 +0530

    task name change

--- accounts/utils.py
+++ accounts/utils.py
@@ -3,7 +3,7 @@ from common.models import TaskLog
 
 def import_in_process_for_user(user_id):
     return TaskLog.objects.filter(
-        name='import_papers_for_user',
+        name='import_user_data',
         args=str((user_id,)),
         status=TaskLog.RUNNING
     ).exists()
